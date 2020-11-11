d67203c0845a6632b65b72e8c762c9b00db4bbee
Zach Myers <zachmyers@woosterbrush.com>
2020-11-11 10:38:58 -0500

    update build script

--- {{cookiecutter.project_slug}}/scripts/build.ps1
+++ {{cookiecutter.project_slug}}/scripts/build.ps1
@@ -3,7 +3,5 @@ Write-Output "Cleaing up build directories"
 Remove-Item .\build\* -Recurse -Force -Confirm:$false
 Remove-Item .\dist\* -Recurse -Force -Confirm:$false
 Write-Output "Building executables"
-poetry run pyinstaller cow_ftp/cli.py --name $NAME --noconfirm --hidden-import=cow_libr --hidden-import=cow_compile --hidden-import=cow_get
-Write-Output "Copying files"
-Copy-Item .\cow_ftp\static .\dist\$NAME\static
+poetry run pyinstaller $NAME/cli.py --name $NAME --noconfirm
 Write-Output "Build complete!"
