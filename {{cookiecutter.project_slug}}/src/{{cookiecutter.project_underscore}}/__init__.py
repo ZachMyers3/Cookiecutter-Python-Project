05a477566e5b9394644e3563f018af6bd5bbf9d3
Zach Myers <zachmyers@woosterbrush.com>
2021-08-17 09:01:33 -0400

    project fixes

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_underscore}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_underscore}}/__init__.py
@@ -3,4 +3,4 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = "{{ cookiecutter.email }}"
 
-from {{cookiecutter.project_underscore}} import main
+from .{{ cookiecutter.project_underscore }} import {{ cookiecutter.project_underscore }}
